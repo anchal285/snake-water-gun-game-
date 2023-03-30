cdc866198fb661c811e7b5f4dcada6fa89d675e8
Anchal <117287003+anchal285@users.noreply.github.com>
2023-03-30 12:06:28 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+let user = prompt("enter s, w or g")
+let cpui = Math.floor(Math.random()*3);
+let cpu = ["s","w","g"][cpui]
+const match = (cpu , user)=>{
+if(cpu == user){
+return "Nobody"
+}
+else if (cpu === "s" && user === "w"){
+return "cpu"
+}
+else if(cpu === "s" && user === "g"){
+return "user"
+}
+else if(cpu === "g" && user === "w"){
+return "user"
+}
+else if(cpu === "g" && user === "s"){
+return "cpu"
+}
+else if(cpu === "w" && user === "g"){
+return "cpu"
+}
+else if(cpu === "w" && user === "s"){
+return "user"
+}
+}
+let result = match(cpu,user)
+document.write(`CPU : ${cpu} <br> user : ${user} <br> The Winner is: ${result.toUpperCase()}`)
